fc8b1e1fa551c0989d12e27fd57f3b42a24da6cc
Podkol <97520637+Podkol@users.noreply.github.com>
2022-06-14 17:26:08 +0300

    число множество чисел деление

--- src/A.java
+++ src/A.java
@@ -1,10 +1,10 @@
 public class A {
     public static void main(String[] args) {
         System.out.println(12);
-        int a=10;
-        int b=15;
-        int c=10;
+        double a=10;
+        double b=15;
+        double c=10;
         System.out.println(a*b);
-
+        System.out.println(a/b);
     }
 }
