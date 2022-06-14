7fd3909dcf152e8033c972b7749ee5a0ad61529c
Podkol <97520637+Podkol@users.noreply.github.com>
2022-06-14 17:23:16 +0300

    число множество чисел

--- src/A.java
+++ src/A.java
@@ -1,5 +1,10 @@
 public class A {
     public static void main(String[] args) {
         System.out.println(12);
+        int a=10;
+        int b=15;
+        int c=10;
+        System.out.println(a*b);
+
     }
 }
